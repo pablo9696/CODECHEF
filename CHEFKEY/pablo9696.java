5b20d74d536aad85ff008e608acec4cd51f6c038
pablo9696 <pperez-96@hotmail.com>
2016-10-27 10:18:17 -0500

    Update pablo9696.java

--- CHEFKEY/pablo9696.java
+++ CHEFKEY/pablo9696.java
@@ -1,1 +1,36 @@
 
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		int[] respuestas = new int[t];
+		
+		for(int z= 0; z< t; z++){
+			
+			int n =  s.nextInt();
+			int m =  s.nextInt();
+			int c =  s.nextInt();
+
+			int contador = 0;
+			int j = 0;
+			for(int i = 1; i<=n; i++){
+				if(c % i ==0){
+					j= (int) (c / i);
+					if(i <= n && j <= m){
+						contador++;
+					}
+				}
+			}	
+			respuestas[z] = contador;
+			n=0;
+			m=0;
+			c=0;
+		}
+		for( int j=0; j < respuestas.length; j++){
+
+			System.out.println(respuestas[j]);			
+		}
+	}
+}
